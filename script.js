c919a0f7cfac7a6c577a7c6256472d5167d0563f
ahmedtolan2019 <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2022-02-20 22:13:00 +0200

    Finnish the generator which the main process of script

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 //user name
-let user = "Ahmed Tolan"
+let user = "Ahmed Tolan";
 
 //data source
 const dataSource = {
-    hold: [true, false],
-    duration: [3, 4, 5, 6, 7],
-    boxCycle: [1, 2, 3, 4, 5, 6, 7]
-}
+  hold: [true, false],
+  duration: [3, 4, 5, 6, 7],
+  boxCycle: [1, 2, 3, 4, 5, 6, 7],
+};
 
 //function that randomly select from an array
-const randomSelect = arr => {
-    let arrLen = arr.length
-    let randomIndex = Math.floor(Math.random() * arrLen)
-    return arr[randomIndex]
-}
+const randomSelect = (arr) => {
+  let arrLen = arr.length;
+  let randomIndex = Math.floor(Math.random() * arrLen);
+  return arr[randomIndex];
+};
 
 //format meditation message input => arr-> [inhale, hold, exhale, boxcycles]
-const formatMsg = arr =>{
-    let inhale = arr[0];
-    let hold = arr[1];
-    let exhale = arr[2];
-    let boxCycle = arr[3];
+const formatMsg = (arr) => {
+  let inhale = arr[0];
+  let hold = arr[1];
+  let exhale = arr[2];
+  let boxCycle = arr[3];
 
-    //inhale for 5 then hold for 2 then exhale for 3 then hold for 6 , then repeat for 4 box cycle
-    let msg = "Inhale for 4 then hold for 4 then exhale for 4 then hold for 4 , then repeat for 4 box cycle"
-    if (hold === 0 ){
-        msg = `Inhale for ${inhale} then exhale for ${exhale} then inhale for ${inhale} then exhale for ${exhale} , then repeat for ${boxCycle} box cycle`
-    }else{
-        msg = `Inhale for ${inhale} then hold for ${hold} then exhale for ${inhale} then hold for ${hold} , then repeat for ${boxCycle} box cycle`
-    }
+  //inhale for 5 then hold for 2 then exhale for 3 then hold for 6 , then repeat for 4 box cycle
+  let msg =
+    "Inhale for 4 then hold for 4 then exhale for 4 then hold for 4 , then repeat for 4 box cycle";
+  if (hold === 0) {
+    msg = `Inhale for ${inhale} then exhale for ${exhale} then inhale for ${inhale} then exhale for ${exhale} , then repeat for ${boxCycle} box cycle`;
+  } else {
+    msg = `Inhale for ${inhale} then hold for ${hold} then exhale for ${inhale} then hold for ${hold} , then repeat for ${boxCycle} box cycle`;
+  }
 
-    return msg
-}
+  return msg;
+};
+
+//main program -- generator
+
+const generator = () => {
+  //iterate throw data store
+  let randoms = {
+    inhale: 0,
+    hold: 0,
+    exhale: 0,
+    boxCycle: 0,
+  };
+  randoms.inhale = randomSelect(dataSource.duration);
+  randoms.hold = randomSelect(dataSource.hold)
+    ? randomSelect(dataSource.duration)
+    : 0;
+  randoms.exhale = randomSelect(dataSource.duration);
+  randoms.boxCycle = randomSelect(dataSource.boxCycle);
+
+  let msg = formatMsg(randoms);
+  console.log(msg);
+};
+
+console.log(`\nWelcome ${user}, let us meditate!\n`);
+generator();
